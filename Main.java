accb54120ede539a57ecc20f58d571bc6b01a632
Wesley Chang <wesley.chang@berkeley.edu>
2025-10-28 10:42:27 -0700

    Enhance game loop with user input and display
    Added input prompts and board display for user interaction.

--- Main.java
+++ Main.java
@@ -7,12 +7,31 @@ public class Main {
     while (!ms.gameOver()) {
       //get input.
       //assume we have i and j from input.
-      ms.clickSquare(i, j);
+      //print the game.
+      //ask for input.
+      for (int i = 0; i < ms.boardHeight; i++) {
+        System.out.print("\n");
+        for (int j = 0; j < ms.boardWidth; j++) {
+          char c = ms.getSquare(i, j);
+          if (c == 'X') {
+            System.out.print(" ");
+          } else {
+            System.out.print(c);
+          }
+        }
+      }
+      System.out.println("which row do you want to click?")
+      int a = System.in();
+      System.out.println("which column do you want to click");
+      int b = System.in();
+      ms.clickSquare(a, b);
     }
     if (ms.gameWon()) {
       //win screen.
+      System.out.println("You win!");
     } else {
       //loss screen.
+      System.out.println("You lose. :\");
 }
 //play again? 
 }
